4fb2079b777728f6a78c41c8be2bd6f3eaf2e66c
vitalh <vitaly@branegy.com>
2019-03-21 12:17:20 +0100

    add SecurityObjectLink

---
+++ dbmaster-api/src/main/java/com/branegy/inventory/model/SecurityObjectLink.java
@@ -0,0 +1,100 @@
+package com.branegy.inventory.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.branegy.dbmaster.core.Project;
+import com.branegy.persistence.custom.BaseCustomEntity;
+import com.branegy.persistence.custom.CustomFieldDiscriminator;
+import com.branegy.persistence.custom.FetchAllObjectIdByProjectSql;
+
+@Entity
+@Table(name = "inv_security_object_link")
+@CustomFieldDiscriminator("SecurityObjectLink")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@FetchAllObjectIdByProjectSql("select id from inv_security_object_link where project_id=:projectId")
+@NamedQueries({
+    @NamedQuery(name=SecurityObjectLink.QUERY_FIND_ALL_SECURITY_OBJECT_LINK_BY_PROJECT, 
+        query="from SecurityObjectLink where project.id=:projectId"),
+    @NamedQuery(name=SecurityObjectLink.QUERY_FIND_ALL_SECURITY_OBJECT_LINK_BY_PROJECT_ID_ACTIVE, 
+        query="from SecurityObjectLink where " + 
+                "project.id=:projectId and "
+                    + "(sourceObject.id=:id or targetObject.id=:id) and "
+                    + "(not(:active) or deleted = false)"),
+})
+public class SecurityObjectLink extends BaseCustomEntity {
+    public static final String QUERY_FIND_ALL_SECURITY_OBJECT_LINK_BY_PROJECT = "SecurityObjectLink.findAllByProject";
+    public static final String QUERY_FIND_ALL_SECURITY_OBJECT_LINK_BY_PROJECT_ID_ACTIVE = "SecurityObjectLink.findAllByProjectIdActive";
+    
+    @ManyToOne(optional = false,fetch=FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name="project_id",nullable=false)
+    Project project;
+    
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="source_object_id",nullable=false)
+    @OnDelete(action=OnDeleteAction.CASCADE)
+    SecurityObject sourceObject;
+    
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="target_object_id",nullable=false)
+    @OnDelete(action=OnDeleteAction.CASCADE)
+    SecurityObject targetObject;
+    
+    @Column(name="IS_AUTO_DETECTED")
+    boolean autodetected;
+    
+    @Column(name="deleted")
+    boolean deleted;
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public SecurityObject getSourceObject() {
+        return sourceObject;
+    }
+
+    public void setSourceObject(SecurityObject sourceObject) {
+        this.sourceObject = sourceObject;
+    }
+
+    public SecurityObject getTargetObject() {
+        return targetObject;
+    }
+
+    public void setTargetObject(SecurityObject targetObject) {
+        this.targetObject = targetObject;
+    }
+
+    public boolean isAutodetected() {
+        return autodetected;
+    }
+
+    public void setAutodetected(boolean autodetected) {
+        this.autodetected = autodetected;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+}
