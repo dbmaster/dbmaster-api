cb6f0b8a8301126107e1caf594a8cf5c66a7da91
vitalh <vitaly@branegy.com>
2019-03-28 10:28:08 +0100

    add subSlice and copyOf for Slice

--- dbmaster-api/src/main/java/com/branegy/service/core/Slice.java
+++ dbmaster-api/src/main/java/com/branegy/service/core/Slice.java
@@ -1,10 +1,25 @@
 package com.branegy.service.core;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.branegy.persistence.util.SliceList;
 
 
 // TODO (Slava) Describe when Slice should be used instead of List
 public interface Slice<E> extends List<E> {
     int getOffset();
     int getTotalSize();
+    
+    public static <E> Slice<E> subSlice(List<E> source, int fromIndex, int toIndex){
+        fromIndex = Math.min(fromIndex,source.size());
+        toIndex = Math.min(toIndex, source.size());
+        return new SliceList<>(source.subList(fromIndex,toIndex), fromIndex, source.size());
+    }
+    
+    public static <E> Slice<E> copyOf(List<E> source, int fromIndex, int toIndex){
+        fromIndex = Math.min(fromIndex,source.size());
+        toIndex = Math.min(toIndex, source.size());
+        return new SliceList<>(new ArrayList<>(source.subList(fromIndex,toIndex)), fromIndex, source.size());
+    }
 }
