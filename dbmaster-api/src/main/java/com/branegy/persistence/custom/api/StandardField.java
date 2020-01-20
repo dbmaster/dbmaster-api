311f3fea30b7a67246536b42559f265ef690c97b
vitalh <vitaly@branegy.com>
2020-01-20 17:16:19 +0100

    fix build

--- dbmaster-api/src/main/java/com/branegy/persistence/custom/api/StandardField.java
+++ dbmaster-api/src/main/java/com/branegy/persistence/custom/api/StandardField.java
@@ -100,9 +100,9 @@ public final class StandardField{
 
     
     /**
-     * @return column expression
+     * @return column name or expression
      */
-    public final String getColumnExpression() {
+    public final String getColumnName() {
         return columnExpression;
     }
     
