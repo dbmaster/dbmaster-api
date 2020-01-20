440873114713c295aad70ce734a2d2b60446706a
vitalh <vitaly@branegy.com>
2020-01-20 16:53:12 +0100

    update SqlCustomSearchImpl for sorting

--- dbmaster-api/src/main/java/com/branegy/persistence/custom/api/StandardField.java
+++ dbmaster-api/src/main/java/com/branegy/persistence/custom/api/StandardField.java
@@ -1,9 +1,29 @@
 package com.branegy.persistence.custom.api;
 
+import static java.util.regex.Pattern.CASE_INSENSITIVE;
+import static java.util.regex.Pattern.DOTALL;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.branegy.service.core.exception.IllegalArgumentApiException;
+
 public final class StandardField{
+    private static final Pattern AS_PATTERN = Pattern.compile("^(?:"
+                                                                + "(?:(.+)\\s+AS\\s+(\\S+))"
+                                                                    + "|"
+                                                                + "(?:[^.()]+\\.([^.()]+))"
+                                                                    + "|"
+                                                                + "([^.()]+)"
+                                                             + ")$",CASE_INSENSITIVE | DOTALL);
     private final String mappingName;
-    private final String columnName;
-    private final String unionName;
+    
+    private final String columnExpression;
+    private final String alias;
+    
+    private final String sortJoinForUnion;
+    private final String sortJoinAliasForUnion;
+    
     private final boolean nullable;
     private final Type type;
     
@@ -11,48 +31,89 @@ public final class StandardField{
         TEXT, BOOLEAN, DOUBLE, LONG, DATE
     }
     
-    private StandardField(String mappingName, String columnName, String unionName, boolean nullable, Type type) {
+    /**
+     * mappingName - business name
+     * columnExpression - name, alias.name, expression as alias
+     * 
+     */
+    private StandardField(String mappingName, String columnExpression, 
+            String sortJoinForUnion, 
+            String sortJoinAliasForUnion, boolean nullable, Type type) {
         this.mappingName = mappingName;
-        this.columnName = columnName;
-        this.unionName = unionName;
+        Matcher matcher = AS_PATTERN.matcher(columnExpression);
+        if (matcher.matches()) {
+            if (matcher.group(1)!=null) {
+                this.alias = matcher.group(2);
+                this.columnExpression = matcher.group(1);
+            } else if (matcher.group(3)!=null) {
+                this.alias = matcher.group(3);
+                this.columnExpression = columnExpression;
+            } else {
+                this.alias = matcher.group(4);
+                this.columnExpression = columnExpression;
+            }
+        } else {
+            this.alias = null;
+            this.columnExpression = columnExpression;
+        }
+        if ((sortJoinForUnion == null) != (sortJoinAliasForUnion == null)) {
+            throw new IllegalArgumentApiException("sortJoin nulls or not nulls");
+        }
+        this.sortJoinForUnion = sortJoinForUnion;
+        this.sortJoinAliasForUnion = sortJoinAliasForUnion;
         this.nullable = nullable;
         this.type = type;
     }
     
     public static StandardField standard(String columnName, Type type) {
-        return new StandardField(null, columnName, null, false, type);
+        return new StandardField(null, columnName, null, null, false, type);
     }
     
     public static StandardField standard(String mappingName, String columnName, Type type) {
-        return standard(mappingName, columnName, null, type);
+        return standard(mappingName, columnName, null, null, type);
     }
     
-    public static StandardField standard(String mappingName, String columnName, String unionName, Type type) {
-        return new StandardField(mappingName, columnName, unionName, false, type);
+    public static StandardField standard(String mappingName, String columnExpression, 
+            String sortJoinForUnion, String sortJoinAliasForUnion, Type type) {
+        return new StandardField(mappingName, columnExpression, sortJoinForUnion, 
+                sortJoinAliasForUnion, false, type);
     }
     
     public static StandardField standardNullable(String columnName, Type type) {
-        return new StandardField(null, columnName, null,  true, type);
+        return new StandardField(null, columnName, null, null,  true, type);
     }
     
     public static StandardField standardNullable(String mappingName, String columnName, Type type) {
-        return standardNullable(mappingName, columnName, null, type);
+        return standardNullable(mappingName, columnName, null, null, type);
     }
     
-    public static StandardField standardNullable(String mappingName, String columnName, String unionName, Type type) {
-        return new StandardField(mappingName, columnName, unionName, true, type);
+    public static StandardField standardNullable(String mappingName, String columnExpression,
+            String sortJoinForUnion,
+            String sortJoinAliasForUnion, Type type) {
+        return new StandardField(mappingName, columnExpression, sortJoinForUnion, sortJoinAliasForUnion,
+                true, type);
     }
     
     public final String getMappingName() {
         return mappingName;
     }
 
-    public final String getColumnName() {
-        return columnName;
+    
+    /**
+     * @return column expression
+     */
+    public final String getColumnExpression() {
+        return columnExpression;
     }
     
-    public final String getUnionName() {
-        return unionName;
+    /**
+     * @return alias only
+     */
+    public final String getAlias() {
+        if (alias == null) {
+            throw new IllegalArgumentApiException("alias is emtpy for "+columnExpression);
+        }
+        return alias;
     }
 
     public final boolean isNullable() {
@@ -61,5 +122,13 @@ public final class StandardField{
 
     public final Type getType() {
         return type;
+    }
+
+    public String getSortJoinForUnion() {
+        return sortJoinForUnion;
+    }
+
+    public String getSortJoinAliasForUnion() {
+        return sortJoinAliasForUnion;
     }
 }
